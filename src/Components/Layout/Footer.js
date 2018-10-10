176d3f9d2b965d9946ccdbf8801c71495d6329ad
TurboTractopelle <44016081+TurboTractopelle@users.noreply.github.com>
2018-10-10 15:25:20 +0200

    add menus

--- src/Components/Layout/Footer.js
+++ src/Components/Layout/Footer.js
@@ -1,5 +1,23 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
 
-const Footer = () => <div>Footer</div>;
+/*
+value={this.state.value}
+onChange={this.handleChange}
+*/
+
+const Footer = () => (
+  <Paper>
+    <Tabs indicatorColor="primary" textColor="primary" centered value={0}>
+      <Tab label="Item One" />
+      <Tab label="Item Two" />
+      <Tab label="Item Three" />
+    </Tabs>
+  </Paper>
+);
 
 export default Footer;
--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,5 +1,19 @@
 import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
 
-const Header = () => <div>header</div>;
+const Header = () => (
+  <AppBar position="static">
+    <Toolbar>
+      <Typography variant="headline" color="inherit">
+        News
+      </Typography>
+      <Button color="inherit">Login</Button>
+    </Toolbar>
+  </AppBar>
+);
 
 export default Header;
