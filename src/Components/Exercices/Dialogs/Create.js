3fbbc8ec4c3a6a18172b7264c8a346dea5e6cb92
TurboTractopelle <44016081+TurboTractopelle@users.noreply.github.com>
2018-10-17 14:57:46 +0200

    icons

--- src/Components/Exercices/Dialogs/Create.js
+++ src/Components/Exercices/Dialogs/Create.js
@@ -6,10 +6,19 @@ import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
+import AddIcon from "@material/icon-button/Add";
 
 const Create = props => (
   <fragment>
-    <Button onClick={this.handleClickOpen}>Open form dialog</Button>
+    <Button
+      onClick={this.handleClickOpen}
+      variant="fab"
+      color="primary"
+      aria-label="Add"
+      size="small"
+    >
+      <AddIcon />
+    </Button>
 
     <Dialog
       open={this.state.open}
@@ -29,3 +38,5 @@ const Create = props => (
     </Dialog>
   </fragment>
 );
+
+export default Create;
--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -4,6 +4,7 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
+import Creat from "../Exercices/Dialogs/Create";
 
 const Header = () => (
   <AppBar position="static">
